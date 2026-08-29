472a08f6019abd579b6267bcfd90ae0f9782fe83
Mohammed said <mohed.said77@gmail.com>
2026-08-29 18:05:41 +0400

    docs: explain welcome loop

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
 
-    // Repeat the loop for the required number of times.
+    // Repeat five times to display each number.
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
