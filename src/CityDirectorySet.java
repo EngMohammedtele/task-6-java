c960e4e5e73849ffd9fd800edcfc087000fd0ba0
Mohammed said <mohed.said77@gmail.com>
2026-08-29 17:46:04 +0400

    docs: add useful comments

--- src/CityDirectorySet.java
+++ src/CityDirectorySet.java
@@ -4,6 +4,7 @@ import java.util.TreeSet;
 public class CityDirectorySet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Keeps city names unique and sorted alphabetically
         TreeSet<String> cities = new TreeSet<>();
 
         // Read the number of cities
